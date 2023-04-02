925ba0e3f2476c43c69fbbfa5c6fd52a22c1b659
wesley <wesley.gomes222@hotmail.com>
2023-04-02 16:51:47 -0300

    criando componente curso

--- app-php/src/app/app.module.ts
+++ app-php/src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { CursoComponent } from './curso/curso.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CursoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
