e95198ebab89ce2fb7091213e6517aef2353d098
wesley <wesley.gomes222@hotmail.com>
2023-04-02 16:34:01 -0300

    criando projeto em angular

--- app-php/src/app/app.component.spec.ts
+++ app-php/src/app/app.component.spec.ts
@@ -1,13 +1,9 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
       declarations: [
         AppComponent
       ],
@@ -20,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projeto-api'`, () => {
+  it(`should have as title 'app-php'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto-api');
+    expect(app.title).toEqual('app-php');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('projeto-api app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('app-php app is running!');
   });
 });
